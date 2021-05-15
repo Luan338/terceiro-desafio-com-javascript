2e2b8e650da91961ed91f65af2e3cda8f36a4140
Luan338 <luan38.eric@gmail.com>
2021-05-15 19:58:34 -0300

    terceiro desafio javascript

---
+++ index.js
@@ -0,0 +1,30 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for(let a=1 ; a<=10 ; a++){
+    console.log(a);
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let n1 = 10;
+
+while(n1 >= 1){
+    console.log(n1);
+    n1--;
+} 
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let impar = 1 ; impar <=100 ; impar += 2){
+    console.log(impar);
+ }
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for(let par = 0 ; par <= 100 ; par +=2){
+    console.log(par);
+}
+
+//5 - Criando tabuada
+//let calc = 5;
+//for(let i = 0; i < 10; i++){
+//    console.log(`${calc} x ${i} = ${calc * i}`);
+//}
+
